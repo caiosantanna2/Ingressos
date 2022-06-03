71c09761c3342a20e6ca9d0d66bae8f962cd5b1c
Caio <caiosantanna2@gmail.com>
2022-06-03 00:08:38 -0300

    Incluindo controller Pessoa.

--- Ingressos.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ingressos.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Ingressos/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Ingressos.Domain.Cliente;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ingressos.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet()]
+        [ProducesResponseType(typeof(Pessoa), 200)]
+        [Route("/pessoa/consultar/{idPessoa}")]
+        public IActionResult consultarPessoa(Pessoa pessoa)
+        {
+            return View();
+        }
+        [HttpPost()]
+        [ProducesResponseType(typeof(Pessoa), 200)]
+        [Route("/pessoa/cadastrar/")]
+        public IActionResult cadastrarPessoa(Pessoa pessoa)
+        {
+            return View();
+        }
+        [HttpPost()]
+        [ProducesResponseType(typeof(Pessoa), 200)]
+        [Route("/pessoa/editar/{idPessoa}")]
+        public IActionResult editarPessoa(Pessoa pessoa)
+        {
+            return View();
+        }
+        [HttpPost()]
+        [ProducesResponseType(typeof(Pessoa), 200)]
+        [Route("/pessoa/excluir/{idPessoa}")]
+        public IActionResult excluirPessoa(Pessoa pessoa)
+        {
+            return View();
+        }
+    }
+}
